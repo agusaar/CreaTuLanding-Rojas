18f892f5f4060fa1a83152e5c1ed0bd56b739448
Agustin Rojas <agusaar@gmail.com>
2024-10-15 15:44:59 -0300

    Included bootstrap

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,14 +1,15 @@
 import CartWidget from "../cartWidget/CartWidget.jsx"
 import beFreeIcon  from './assets/beFreeIcon.png'
+import Button from 'react-bootstrap/Button'
 
 const NavBar = () =>{
     return (
         <nav className="navBar">
             <img className="logo" src={beFreeIcon} alt="BeFree - Icon"/>
-            <button>Remeras</button>
-            <button>Buzos</button>
-            <button>Pantalones</button>
-            <button>Camperas</button>
+            <Button>Remeras</Button>
+            <Button>Buzos</Button>
+            <Button>Pantalones</Button>
+            <Button>Camperas</Button>
             <CartWidget />
         </nav>
     )
